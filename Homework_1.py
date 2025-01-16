f96b21765b5218fbf0d2fc11da479741f751663c
amakushkin <sahskamak@mail.ru>
2025-01-17 00:12:02 +0300

    Task_4 Полевые испытания

--- Homework_1.py
+++ Homework_1.py
@@ -11,6 +11,18 @@ def mean(numbers):
         return 0
 
 
+def courses_rating(persons, course):
+    res = 0
+    lenth = 0
+    for person in persons:
+        if course in person.grades:
+            if person.grades[course]:
+                res += mean(person.grades[course])
+                lenth += 1
+    if lenth != 0:
+        return res/lenth
+
+
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -19,14 +31,19 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-    def rate_lecturer(self, lecturer, grade):
-        if isinstance(lecturer, Lecturer) and self.courses_in_progress and lecturer.courses_attached:
+
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer,Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
             if isinstance(grade, int) and (0 <= grade <= 10) :
-                lecturer.grades += [grade]
+                if course in lecturer.grades:
+                    lecturer.grades[course] += [grade]
+                else:
+                    lecturer.grades[course] = [grade]
             else:
                 print("Оценка введена неверно")
         else:
             return 'Ошибка'
+
     def __str__(self):
         info = str(f"Имя: {self.name}\n"
                    f"Фамилия: {self.surname}\n"
@@ -54,7 +71,7 @@ class Mentor:
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
-        self.grades = []
+        self.grades = {}
     def __str__(self):
         return f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка: {mean(self.grades)}"
     def __lt__(self, other):
@@ -80,43 +97,46 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
-student_1 = Student('Ruoy', 'Eman', 'your_gender')
-student_1.courses_in_progress += ['Python']
-student_1.courses_in_progress += ['Git']
-student_1.finished_courses += ['1C']
+students = [Student('Ruoy', 'Eman', 'your_gender'),
+            Student('Boris', 'Yeltsin', 'your_gender')]
+students[0].courses_in_progress += ['Python']
+students[0].courses_in_progress += ['Git']
+students[0].finished_courses += ['1C']
+students[1].courses_in_progress += ['Python']
+students[1].finished_courses += ['Exel']
+students[1].finished_courses += ['PHP']
 
-student_2 = Student('Ron', 'Wuizli', 'your_gender')
-student_2.courses_in_progress += ['Python']
-student_2.finished_courses += ['Exel']
-student_2.finished_courses += ['PHP']
+lecturers = [Lecturer('Ivan', 'Ivanov'),
+            Lecturer('Nikolay', 'Valuev')]
+lecturers[0].courses_attached += ['Python']
+lecturers[0].courses_attached += ['Java']
+lecturers[1].courses_attached += ['JS']
 
-lecturer_1 = Lecturer('Ivan', 'Ivanov')
-lecturer_1.courses_attached += ['Python']
-lecturer_1.courses_attached += ['Java']
+reviewers = [Reviewer('Petr', 'Petrov'),
+             Reviewer('Ivan', 'Grozny')]
+reviewers[0].courses_attached += ['Python']
+reviewers[0].courses_attached += ['Git']
+reviewers[1].courses_attached += ['Calc']
+reviewers[1].courses_attached += ['Word']
 
-lecturer_2 = Lecturer('Nikolay', 'Valuev')
-lecturer_2.courses_attached += ['JS']
+reviewers[0].rate_hw(students[0], 'Python', 7)
+reviewers[0].rate_hw(students[0], 'Python', 10)
+reviewers[0].rate_hw(students[0], 'Git', 10)
+reviewers[1].rate_hw(students[1], 'Python', 9)
+reviewers[1].rate_hw(students[1], 'Python', 8)
 
-reviewer_1 = Reviewer('Petr', 'Petrov')
-reviewer_1.courses_attached += ['Python']
-reviewer_1.courses_attached += ['Git']
+students[0].rate_lecturer(lecturers[0], 'Python', 10)
+students[0].rate_lecturer(lecturers[0], 'Python', 8)
+students[1].rate_lecturer(lecturers[0], 'Python', 9)
+students[1].rate_lecturer(lecturers[1], 'Java', 5)
 
-reviewer_1.rate_hw(student_1, 'Python', 7)
-reviewer_1.rate_hw(student_1, 'Python', 10)
-reviewer_1.rate_hw(student_1, 'Git', 10)
-reviewer_1.rate_hw(student_2, 'Python', 9)
-reviewer_1.rate_hw(student_2, 'Python', 8)
+print(f"reviewers[0]: {reviewers[0]} ")
+print(f"lecturers[0]: {lecturers[0]} ")
+print(f"lecturers[1]: {lecturers[1]} ")
+print(f"students[0]: {students[0]} ")
+print(f"students[1]: {students[1]} ")
 
-student_1.rate_lecturer(lecturer_1, 10)
-student_1.rate_lecturer(lecturer_1, 8)
-student_2.rate_lecturer(lecturer_1, 9)
-student_1.rate_lecturer(lecturer_2, 5)
-
-print(f"reviewer_1: {reviewer_1} ")
-print(f"lecturer_1: {lecturer_1} ")
-print(f"lecturer_2: {lecturer_2} ")
-print(f"student_1: {student_1} ")
-print(f"student_2: {student_2} ")
-
-print("lecturer_1 < lecturer_2 ==>", lecturer_1 > lecturer_2)
-print("student_1 > student_2 ==>", student_1 > student_2)
+print("lecturers[0] < lecturers[1] ==>", lecturers[0] > lecturers[1])
+print("students[0] > students[1] ==>", students[0] > students[1])
+print(f"\nСредняя оценка домашних работ по Python: {courses_rating(students, 'Python')}")
+print(f"Средняя оценка лекторов по курсу Python: {courses_rating(lecturers, 'Python')}")
